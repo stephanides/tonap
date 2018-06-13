c1f945355d2e50ff45ef83e2a586930d48deca02
Štefan Mačuga <stefanmacuga1@gmail.com>
2018-06-13 19:23:54 +0200

    added contact container, map

--- src/public/js/tonap.js
+++ src/public/js/tonap.js
@@ -19,8 +19,6 @@ function showAllProducts(){
       setTimeout(function(){
         document.getElementById("moreProducts").style.overflow = "unset";
       },500);
-     // document.getElementById("moreProducts").style.height = "auto";
-      //document.getElementById("moreProducts").style.overflow = "unset";
       isopen = true;
     }
     else{
@@ -30,4 +28,34 @@ function showAllProducts(){
       isopen = false;
     }
   });
+}
+function loadMap(){
+  var mapOptions = {
+    center: new google.maps.LatLng(48.635896, 21.714897),
+    zoom: 15,
+    mapTypeControl: false,
+    fullscreenControl:false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  loadJSON(function(response) {
+		var loaded_json = JSON.parse(response);
+		var styledMapType = new google.maps.StyledMapType(loaded_json, {name: "Map"});
+
+		map.mapTypes.set("styled_map", styledMapType);
+		map.setMapTypeId("styled_map");
+	});
+
+	function loadJSON(callback) {
+		var xobj = new XMLHttpRequest();
+		xobj.overrideMimeType("application/json");
+		xobj.open("GET", window.location.origin + "/assets/js/map.json", true);
+		xobj.onreadystatechange = function () {
+			if (xobj.readyState == 4 && xobj.status == "200") {
+				callback(xobj.responseText);
+			}
+		};
+		xobj.send(null);
+	}
+
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
